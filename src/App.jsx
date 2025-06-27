6628f2291d516543527f798363ad31627eddf5a2
AfrideHossain <afredehossain@gmail.com>
2025-06-27 09:59:18 +0600

    Add Hero component with GSAP animations and update App structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 // register gsap plugins
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -9,6 +10,9 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      {/* temporary div */}
+      <div className="min-h-screen"></div>
     </main>
   );
 };
